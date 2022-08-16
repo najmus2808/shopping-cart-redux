1331244b3728889a761ae05c9eb3fab3537d99bb
najmus2808 <najmus2808@gmail.com>
2022-08-16 09:57:01 +0600

    shopping cart done

--- src/redux/shoppingCart/helper.js
+++ src/redux/shoppingCart/helper.js
@@ -1,61 +1,65 @@
 export const addHandler = (state, id) => {
-    const stateObj = { ...state }
-    const item = stateObj.products.find(item => item.id === id);
-    console.log("add", id)
-    console.log("add", stateObj.cartItems)
+  const stateObj = { ...state };
+  const item = stateObj.products.find((item) => item.id === id);
+  console.log("add", id);
+  console.log("add", stateObj.cartItems);
 
-    const isExist = stateObj.cartItems.findIndex(item => item.id === id)
-    console.log("add", isExist)
+  const isExist = stateObj.cartItems.findIndex((item) => item.id === id);
+  console.log("add", isExist);
 
-    let objIndex = [...stateObj.products].findIndex((item => item.id === id));
-    stateObj.products[objIndex].stockQuantity = stateObj.products[objIndex].stockQuantity - 1
+  let objIndex = [...stateObj.products].findIndex((item) => item.id === id);
+  stateObj.products[objIndex].stockQuantity =
+    stateObj.products[objIndex].stockQuantity - 1;
 
-    if (isExist === -1) {
-        return {
-            ...stateObj,
-            cartItems: [...stateObj.cartItems, { ...item, quantity: 1 }]
-        }
-    } else {
-        stateObj.cartItems[objIndex].quantity = stateObj.cartItems[objIndex]?.quantity + 1
-        return {
-            ...stateObj
-        }
-
-    }
-
-
-}
+  if (isExist === -1) {
+    return {
+      ...stateObj,
+      cartItems: [...stateObj.cartItems, { ...item, quantity: 1 }],
+    };
+  } else {
+    let objIndex = [...stateObj.cartItems].findIndex((item) => item.id === id);
+    stateObj.cartItems[objIndex].quantity =
+      stateObj.cartItems[objIndex]?.quantity + 1;
+    return {
+      ...stateObj,
+    };
+  }
+};
 
 export const incrementHandler = (state, id) => {
-    const stateObj = { ...state }
-    let objIndex = [...stateObj.cartItems].findIndex((item => item.id === id));
-    stateObj.cartItems[objIndex].quantity = stateObj.cartItems[objIndex].quantity + 1;
-    stateObj.products[objIndex].stockQuantity = stateObj.products[objIndex].stockQuantity - 1
+  const stateObj = { ...state };
+  let objIndex = [...stateObj.cartItems].findIndex((item) => item.id === id);
+  let productIndex = [...stateObj.products].findIndex((item) => item.id === id);
+  stateObj.cartItems[objIndex].quantity =
+    stateObj.cartItems[objIndex].quantity + 1;
+  stateObj.products[productIndex].stockQuantity =
+    stateObj.products[productIndex].stockQuantity - 1;
 
-    return {
-        ...stateObj,
-
-    }
-}
+  return {
+    ...stateObj,
+  };
+};
 
 export const decrementHandler = (state, id) => {
-    const stateObj = { ...state }
-    let objIndex = [...stateObj.cartItems].findIndex((item => item.id === id));
-    stateObj.cartItems[objIndex].quantity = stateObj.cartItems[objIndex].quantity - 1;
-    stateObj.products[objIndex].stockQuantity = stateObj.products[objIndex].stockQuantity + 1
+  const stateObj = { ...state };
+  let objIndex = [...stateObj.cartItems].findIndex((item) => item.id === id);
+  let productIndex = [...stateObj.products].findIndex((item) => item.id === id);
+  stateObj.cartItems[objIndex].quantity =
+    stateObj.cartItems[objIndex].quantity - 1;
+  stateObj.products[productIndex].stockQuantity =
+    stateObj.products[productIndex].stockQuantity + 1;
 
-
-    if (stateObj.cartItems[objIndex].quantity < 1) {
-        const modifiedCartItems = stateObj?.cartItems?.filter((item) => item?.id !== id);
-        return {
-            ...stateObj,
-            cartItems: [...modifiedCartItems]
-        }
-    }
-
+  if (stateObj.cartItems[objIndex].quantity < 1) {
+    const modifiedCartItems = stateObj?.cartItems?.filter(
+      (item) => item?.id !== id
+    );
     return {
-        ...stateObj,
+      ...stateObj,
+      cartItems: [...modifiedCartItems],
+    };
+  }
 
-    }
-
-}
+  return {
+    ...stateObj,
+  };
+};
