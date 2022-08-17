9967c9de756020947b3e87098b09b6f9bfdfc131
najmus2808 <najmus2808@gmail.com>
2022-08-17 21:23:17 +0600

    shopping cart project

--- src/redux/shoppingCart/helper.js
+++ src/redux/shoppingCart/helper.js
@@ -6,17 +6,17 @@ export const addHandler = (state, id) => {
   const isExist = cartItemsNew.findIndex((item) => item.id === id);
 
   if (isExist === -1) {
-    let objIndex = productsNew.findIndex((item) => item.id === id);
-    productsNew[objIndex].stockQuantity =
-      productsNew[objIndex].stockQuantity - 1;
+    let productIndex = productsNew.findIndex((item) => item.id === id);
+    productsNew[productIndex].stockQuantity =
+      productsNew[productIndex].stockQuantity - 1;
     return {
       ...stateObj,
       products: [...productsNew],
       cartItems: [{ ...item, quantity: 1 }, ...cartItemsNew],
     };
   } else {
-    let objIndex = cartItemsNew.findIndex((item) => item.id === id);
-    cartItemsNew[objIndex].quantity = cartItemsNew[objIndex]?.quantity + 1;
+    let cartItemIndex = cartItemsNew.findIndex((item) => item.id === id);
+    cartItemsNew[cartItemIndex].quantity = cartItemsNew[cartItemIndex]?.quantity + 1;
     let productIndex = productsNew.findIndex((item) => item.id === id);
     productsNew[productIndex].stockQuantity =
       productsNew[productIndex].stockQuantity - 1;
@@ -32,9 +32,9 @@ export const incrementHandler = (state, id) => {
   const stateObj = { ...state };
   const productsNew = [...stateObj.products];
   const cartItemsNew = [...stateObj.cartItems];
-  let objIndex = cartItemsNew.findIndex((item) => item.id === id);
+  let cartItemIndex = cartItemsNew.findIndex((item) => item.id === id);
   let productIndex = productsNew.findIndex((item) => item.id === id);
-  cartItemsNew[objIndex].quantity = cartItemsNew[objIndex].quantity + 1;
+  cartItemsNew[cartItemIndex].quantity = cartItemsNew[cartItemIndex].quantity + 1;
   productsNew[productIndex].stockQuantity =
     productsNew[productIndex].stockQuantity - 1;
 
@@ -49,13 +49,13 @@ export const decrementHandler = (state, id) => {
   const stateObj = { ...state };
   const productsNew = [...stateObj.products];
   const cartItemsNew = [...stateObj.cartItems];
-  let objIndex = cartItemsNew.findIndex((item) => item.id === id);
-  let productIndex = [...productsNew].findIndex((item) => item.id === id);
-  cartItemsNew[objIndex].quantity = cartItemsNew[objIndex].quantity - 1;
+  let cartItemIndex = cartItemsNew.findIndex((item) => item.id === id);
+  let productIndex = productsNew.findIndex((item) => item.id === id);
+  cartItemsNew[cartItemIndex].quantity = cartItemsNew[cartItemIndex].quantity - 1;
   productsNew[productIndex].stockQuantity =
     productsNew[productIndex].stockQuantity + 1;
 
-  if (cartItemsNew[objIndex].quantity < 1) {
+  if (cartItemsNew[cartItemIndex].quantity < 1) {
     const modifiedCartItems = cartItemsNew.filter((item) => item?.id !== id);
     return {
       ...stateObj,
